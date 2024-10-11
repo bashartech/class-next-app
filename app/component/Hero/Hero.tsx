181187babf991676abb33df291602b9e8db2cbc3
bashartech <144168218+bashartech@users.noreply.github.com>
2024-10-12 00:54:41 +0500

    second commit

--- app/component/Hero/Hero.tsx
+++ app/component/Hero/Hero.tsx
@@ -3,17 +3,15 @@ import Link from "next/link";
 // Define an interface for the props to specify the expected types
 interface HeroProps {
   heading: string;
-  About: string;
   btn: string;
 }
 
-export default function Hero({ heading, About, btn }: HeroProps) {
+export default function Hero({ heading, btn }: HeroProps) {
   return (
     <div>
       <section className="hero-section">
         <div className="background-image"></div>
         <div className="container">
-          <h2>{About}</h2>
           <h1 className="headline">{heading}</h1>
           <p className="subheading">
             Expert IT services for startups and enterprises, driving digital transformation and growth
