217211ff34fe0e5a73b081c488de821091025c19
bashartech <144168218+bashartech@users.noreply.github.com>
2024-10-12 00:44:42 +0500

    second commit

--- app/component/Hero/Hero.tsx
+++ app/component/Hero/Hero.tsx
@@ -1,18 +1,29 @@
-import Link from "next/link"
-export default function Hero(props :any) {
-console.log(props, "props")
-      
+import Link from "next/link";
+
+// Define an interface for the props to specify the expected types
+interface HeroProps {
+  heading: string;
+  About: string;
+  btn: string;
+}
+
+export default function Hero({ heading, About, btn }: HeroProps) {
   return (
     <div>
       <section className="hero-section">
         <div className="background-image"></div>
         <div className="container">
-        <h2>{props.About}</h2>
-            <h1 className="headline">{props.heading}</h1>
-            <p className="subheading">Expert IT services for startups and enterprises, driving digital transformation and growth</p>
-          <Link href = "/services"><button className="cta-button">{props.btn}</button></Link>
+          <h2>{About}</h2>
+          <h1 className="headline">{heading}</h1>
+          <p className="subheading">
+            Expert IT services for startups and enterprises, driving digital transformation and growth
+          </p>
+          <Link href="/services">
+            <button className="cta-button">{btn}</button>
+          </Link>
         </div>
-    </section>
+      </section>
     </div>
-  )
+  );
 }
+
