ea0909397c3b55d388332ad36e9d6ec64629f3fc
Bashar Sheikh <144168218+bashartech@users.noreply.github.com>
2024-10-24 00:59:55 +0500

    Update Hero.tsx

--- app/component/Hero/Hero.tsx
+++ app/component/Hero/Hero.tsx
@@ -1,27 +1,27 @@
 import Link from "next/link";
-
+import cssStyle from "./hero.module.css"
 // Define an interface for the props to specify the expected types
 interface HeroProps {
   heading: string;
-  
 }
 
-export default function Hero({ heading }: HeroProps) {
+export default function Hero({ heading}: HeroProps) {
   return (
     <div>
-      <section className="hero-section">
-        <div className="background-image"></div>
-        <div className="container">
-          <h1 className="headline">{heading}</h1>
-          <p className="subheading">
+      <section className={cssStyle.heroSection}>
+        <div className={cssStyle.backgroundImage}></div>
+        <div className={cssStyle.container}>
+          <h1 className={cssStyle.headline}>{heading}</h1>
+          <p className={cssStyle.subheading}>
             Expert IT services for startups and enterprises, driving digital transformation and growth
           </p>
           <Link href="/services">
-            <button className="cta-button">Contact</button>
+            <button className={cssStyle.ctaButton}>Contact</button>
           </Link>
         </div>
       </section>
     </div>
   );
 }
+
 
